68dff6d3005594585e16e1ffda9abea18b216c2d
JunbingGuo <guojunbingsdu@163.com>
2019-09-27 18:41:20 +0800

    feat:暂存

--- es6/greeter.js
+++ es6/greeter.js
@@ -1,5 +1,19 @@
+// function greeter(person: string) {
+//   return "Hello, " + person;
+// }
+var Student = /** @class */ (function () {
+    function Student(firstName, middleIntial, lastName) {
+        this.firstName = firstName;
+        this.middleIntial = middleIntial;
+        this.lastName = lastName;
+        this.fullName = firstName + ' ' + middleIntial + ' ' + lastName;
+    }
+    return Student;
+}());
 function greeter(person) {
-    return "Hello, " + person;
+    return "Hello, " + person.firstName + ' ' + person.lastName;
 }
-var user = [1, 3, 4];
+// let user = { firstName: "Jane", lastName: "User" };
+var user = new Student("Jane", "M.", "User");
+console.log(user);
 document.body.innerHTML = greeter(user);
--- es6/greeter.ts
+++ es6/greeter.ts
@@ -1,7 +1,30 @@
-function greeter(person: string) {
-  return "Hello, " + person;
+// function greeter(person: string) {
+//   return "Hello, " + person;
+// }
+
+// let user = [1, 3, 4];
+
+// document.body.innerHTML = greeter(user);
+
+interface Person {
+  firstName: String;
+  lastName: String;
 }
 
-let user = [1, 3, 4];
+class Student {
+  fullName: String;
+  constructor(public firstName, public middleIntial, public lastName) {
+    this.fullName = firstName + ' ' + middleIntial + ' ' + lastName;
+  }
+}
+
+function greeter(person: Person) {
+  return "Hello, " + person.firstName + ' ' + person.lastName;
+}
+// let user = { firstName: "Jane", lastName: "User" };
+
+let user = new Student("Jane", "M.", "User");
+
+console.log(user);
 
 document.body.innerHTML = greeter(user);
